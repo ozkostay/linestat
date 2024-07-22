914dec313be4847b8f20e1fd84ee6c7c794f91a5
ozkostay <ozkostay@mail.ru>
2024-07-22 20:32:47 +0500

    home 220724

--- backend/src/tennis/db/db.module.ts
+++ backend/src/tennis/db/db.module.ts
@@ -1,20 +1,21 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
-    imports: [
-      TypeOrmModule.forRoot({
-        type: 'postgres',
-        host: '192.168.15.250',
-        port: 5432,
-        username: 'linestatuser',
-        password: 'linestatcrypt',
-        database: 'tennis',
-        entities: [],
-        synchronize: true,
-      }),
-    ],
-  })
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_URL,
+      port: 5432,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: 'tennis',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
+})
 export class DbModule {}
-
-
