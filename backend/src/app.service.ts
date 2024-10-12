11b4a9badc97445fa8c990aa380f1d9710b97408
ozkostay <ozkostay@mail.ru>
2024-10-12 18:11:36 +0500

    log 3000

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    console.log('Hello World! I am LINESTAT!!!');
     return 'Hello World! I am LINESTAT!!!';
   }
 }
